01f4f8a1b9c3307083398febca5a35f887426f6f
adaltoss <adaltoss@192.168.0.100>
2021-03-18 21:56:28 -0300

    Aula04 - 18/03/2021 - Encapsulamento

--- AulaOO/src/aulaoo/Cliente.java
+++ AulaOO/src/aulaoo/Cliente.java
@@ -1,14 +1,91 @@
 package aulaoo;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author adaltoss
  */
 public class Cliente {
+    
+    public static int MAIORIDADE = 18;
      
     public int id;
     public String nome, endereco, email;
+    private int idade;
+    protected String cpf;
     public Cidade municipio;
+    
+    
+    public static double calcularEndividamentoMaximo(double valorHora, double horas){
+        double salario = valorHora * horas;
+        return (salario * 0.3);
+    }
+    
+    
+    
+    public int getIdade(){
+        return idade;
+    }
+    
+    public void setIdade(int idade){
+        if( idade >= MAIORIDADE ){
+            this.idade = idade;
+        }else{
+            JOptionPane.showMessageDialog(null, "A idade não pode ser inferior a 18!");
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Cidade getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(Cidade municipio) {
+        this.municipio = municipio;
+    }
+    
+    
+    
     
     
 }
--- AulaOO/src/aulaoo/Main.java
+++ AulaOO/src/aulaoo/Main.java
@@ -10,35 +10,44 @@ public class Main {
     
     public static void main(String[] args) {
         
-        int x = 0;
+//        int x = 0;
+//        
+//        Cidade cid2 = new Cidade("Alvorada");
+//    //    System.out.println("Nome do cid2: " + cid2.nome);
+//        
+//        Cidade cid3 = new Cidade(10);
+//        Cidade cid4 = new Cidade("Capão da Canoa");
+//        Cidade cid5 = new Cidade(12, "Cacequi");
+//        Cidade cid6 = new Cidade("Camaquâ", 15);
+//        
+//        cid5.imprimirCidade();
+//        
+//        System.out.println("--------------");
+//        
+//        cid5.nome = "Rosário do Sul";
+//        System.out.println("Cidade: \n" + cid5.retornarCidade() );
+//        
+//        System.out.println("--------------");
+//        System.out.println("Cidade: \n" + cid6.retornarCidade() );
+//        
+//        Cidade cid = new Cidade();
+//        cid.id = 25;
+//        cid.nome = "Porto Alegre";
+//        
+//        Cliente cli = new Cliente();
+//        cli.id = 1;
+//        cli.nome = "João";
+//        cli.endereco = "Av. Ipiranga, 1200";
+//        cli.municipio = cid;
         
-        Cidade cid2 = new Cidade("Alvorada");
-    //    System.out.println("Nome do cid2: " + cid2.nome);
+            System.out.println("Valor máximo da parcela: " +  
+                    Cliente.calcularEndividamentoMaximo(10, 180) );
         
-        Cidade cid3 = new Cidade(10);
-        Cidade cid4 = new Cidade("Capão da Canoa");
-        Cidade cid5 = new Cidade(12, "Cacequi");
-        Cidade cid6 = new Cidade("Camaquâ", 15);
-        
-        cid5.imprimirCidade();
-        
-        System.out.println("--------------");
-        
-        cid5.nome = "Rosário do Sul";
-        System.out.println("Cidade: \n" + cid5.retornarCidade() );
-        
-        System.out.println("--------------");
-        System.out.println("Cidade: \n" + cid6.retornarCidade() );
-        
-        Cidade cid = new Cidade();
-        cid.id = 25;
-        cid.nome = "Porto Alegre";
-        
-        Cliente cli = new Cliente();
-        cli.id = 1;
-        cli.nome = "João";
-        cli.endereco = "Av. Ipiranga, 1200";
-        cli.municipio = cid;
+          Cliente cli2 = new Cliente();
+          cli2.nome = "João";
+          cli2.setEmail( "joão@gmail.com" );
+          cli2.setIdade(20);
+          
         
 //        System.out.println("Nome: " + cli.nome 
 //                + "\nEnd: "  + cli.endereco + 
